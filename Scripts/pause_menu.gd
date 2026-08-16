f6909f31fe9af19587b4b74b91f814ebdc45aa4b
soggyricecake <kion.h.y.lee@outlook.com>
2026-08-16 18:50:00 +0100

    added basic functionality to ui scenes

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,13 +1,16 @@
 extends Control
 
 
-
-func _on_play_pressed() -> void:
+func _on_resume_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_game.tscn")
 
 
 func _on_settings_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/UI/settings.tscn")
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/UI/title_screen.tscn")
 
 
 func _on_quit_pressed() -> void:
