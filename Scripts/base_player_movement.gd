cbbe96ca8ab1be2196bb00c48620a2662b20bfa1
josh <20davisj@nhgs.calderdale.sch.uk>
2026-08-16 01:50:05 +0100

    BaseCode
    This is the base code for the project

---
+++ Scripts/base_player_movement.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var yDirection := Input.get_axis("moveup", "movedown")
+	var xDirection := Input.get_axis("moveleft", "moveright")
+	
+	if xDirection:
+		velocity.x = xDirection * SPEED
+	else:
+		velocity.x = 0
+	
+	
+	if yDirection:
+		velocity.y = yDirection * SPEED
+	else:
+		velocity.y = 0	
+
+	move_and_slide()
